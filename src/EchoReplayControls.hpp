d2ef44052bc45b0b0a76505f3b909114e9f9969f
doonchy16-cloud <doonchy16@gmail.com>
2026-08-30 12:41:42 -0700

    v1.1: expand Replay Studio control surface and PB truth labels

--- src/EchoReplayControls.hpp
+++ src/EchoReplayControls.hpp
@@ -27,18 +27,29 @@ public:
 
     void refresh();
     void closeStudio();
+    void setProgressContext(
+        float gdLevelBestPercent,
+        float bestRecordedPercent,
+        float sessionBestPercent,
+        bool platformer
+    );
+    void setLauncherScale(float scale);
+    void setReplayReadyHintEnabled(bool enabled);
 
     [[nodiscard]] bool isStudioOpen() const;
 
 private:
     void onOpen(cocos2d::CCObject* sender);
     void onClose(cocos2d::CCObject* sender);
+    void onPreviousAttempt(cocos2d::CCObject* sender);
+    void onNextAttempt(cocos2d::CCObject* sender);
     void onPlayPause(cocos2d::CCObject* sender);
     void onRestart(cocos2d::CCObject* sender);
     void onPreviousFrame(cocos2d::CCObject* sender);
     void onNextFrame(cocos2d::CCObject* sender);
     void onSpeed(cocos2d::CCObject* sender);
     void onCamera(cocos2d::CCObject* sender);
+    void onSettings(cocos2d::CCObject* sender);
     void onScrub(cocos2d::CCObject* sender);
 
     void setStudioOpen(bool open);
@@ -46,11 +57,20 @@ private:
     EchoReplaySession* m_session = nullptr;
     StudioModeCallback m_onStudioModeChanged;
     bool m_studioOpen = false;
+    bool m_hasOpenedStudio = false;
+    bool m_replayReadyHintEnabled = true;
+    bool m_platformer = false;
+    float m_launcherScale = 1.0f;
+    float m_gdLevelBestPercent = 0.0f;
+    float m_bestRecordedPercent = 0.0f;
+    float m_sessionBestPercent = 0.0f;
 
     CCMenuItemSpriteExtra* m_launcher = nullptr;
     cocos2d::CCNode* m_panel = nullptr;
     Slider* m_slider = nullptr;
 
+    cocos2d::CCLabelBMFont* m_hintLabel = nullptr;
+    cocos2d::CCLabelBMFont* m_truthLabel = nullptr;
     cocos2d::CCLabelBMFont* m_attemptLabel = nullptr;
     cocos2d::CCLabelBMFont* m_timeLabel = nullptr;
     cocos2d::CCLabelBMFont* m_progressLabel = nullptr;
