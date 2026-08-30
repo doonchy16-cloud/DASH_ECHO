1f0783eab92666b71a40cd69c44346193f218a99
doonchy16-cloud <doonchy16@gmail.com>
2026-08-30 00:30:01 -0700

    v0.7: define replay session renderer

---
+++ src/EchoReplaySession.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "EchoGhost.hpp"
+#include "EchoReplayTimeline.hpp"
+
+#include <cstdint>
+
+namespace cocos2d {
+class CCNode;
+}
+
+namespace dash_echo {
+
+class EchoReplaySession final {
+public:
+    static constexpr std::uint8_t kReplayOpacity = 176;
+
+    bool attach(cocos2d::CCNode* parent, int zOrder);
+    void detach();
+
+    bool load(AttemptRecord const& attempt, AttemptHistoryEntry const& history);
+    void start();
+    void advance(float dt);
+    void restart();
+    void stop();
+    void clear();
+
+    [[nodiscard]] bool isAttached() const;
+    [[nodiscard]] bool isLoaded() const;
+    [[nodiscard]] bool isPlaying() const;
+    [[nodiscard]] EchoReplayTimeline const& timeline() const;
+    [[nodiscard]] EchoReplayTimeline& timeline();
+
+private:
+    void bindGhostToOwnedClip();
+
+    EchoReplayTimeline m_timeline;
+    EchoGhost m_ghost;
+};
+
+} // namespace dash_echo
